73cdfdc4f0a8c109a60fd6faadc5419fbc9dfd3c
nimkoes <xxxelppa@gmail.com>
2021-05-01 00:32:32 +0900

    feat : 자동차의 이동 거리 확인 기능 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,14 +1,21 @@
 package racingcar;
 
 public class Car {
+    private int moveDistance;
+    
     public CarAction action(int randomNumber) {
         if(getAction(randomNumber)) {
             return CarAction.STOP;
         }
+        ++this.moveDistance;
         return CarAction.GO;
     }
     
     private boolean getAction(int actionNumber) {
         return actionNumber <= 3;
+    }
+    
+    public int moveDistance() {
+        return this.moveDistance;
     }
 }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,13 +8,30 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
     
+    Car car;
+    
+    @BeforeEach
+    void setUp() {
+        car = new Car();
+    }
+    
     @Test
     @DisplayName("GO, STOP 테스트")
-    public void carAction() {
-        Car car = new Car();
+    void carAction() {
         assertThat(car.action(1)).isEqualTo(CarAction.STOP);
         assertThat(car.action(3)).isEqualTo(CarAction.STOP);
         assertThat(car.action(4)).isEqualTo(CarAction.GO);
         assertThat(car.action(9)).isEqualTo(CarAction.GO);
+    }
+    
+    @Test
+    @DisplayName("이동 거리 확인")
+    void distance() {
+        car.action(1);
+        car.action(4);
+        car.action(3);
+        car.action(5);
+        
+        assertThat(car.moveDistance()).isEqualTo(2);
     }
 }
