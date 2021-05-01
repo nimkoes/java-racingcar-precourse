2c87ac39cc8826bca4412cdfe4dae13c3faff3ff
nimkoes <xxxelppa@gmail.com>
2021-05-01 09:28:09 +0900

    feat : 이동 횟수에 대한 유효성 검사 기능 추가
    숫자를 입력 하였는지 확인
    양수를 입력 하였는지 확인

--- src/main/java/racingcar/ValidationUtil.java
+++ src/main/java/racingcar/ValidationUtil.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class ValidationUtil {
     
     static final int MAX_NAME_LENGTH = 5;
+    static final String REG_EXP_ONLY_NUMBER = "^[0-9]+$";
     
     public static boolean validName(List<String> participants) {
         return !participants.isEmpty()
@@ -26,4 +27,15 @@ public class ValidationUtil {
         return maxLength > MAX_NAME_LENGTH;
     }
     
+    public static boolean validMoveCount(String userInput) {
+        return isOnlyNumber(userInput) && isPositiveNumber(userInput);
+    }
+    
+    private static boolean isPositiveNumber(String userInput) {
+        return Integer.parseInt(userInput) >= 1;
+    }
+    
+    private static boolean isOnlyNumber(String userInput) {
+        return userInput.matches(REG_EXP_ONLY_NUMBER);
+    }
 }
--- src/test/java/racingcar/ValidationUtilTest.java
+++ src/test/java/racingcar/ValidationUtilTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -13,11 +14,18 @@ public class ValidationUtilTest {
     
     
     @ParameterizedTest
-    @CsvSource(value = {",:false", "붕붕이,빵빵이,빙봉:true", "빙봉,빙봉:false", "붕붕,빙봉,붕붕:false", "프리코스:true", "myFancyCar:false", ",super:false"}, delimiter = ':')
+    @CsvSource(value = {"'':false", ",:false", "붕붕이,빵빵이,빙봉:true", "빙봉,빙봉:false", "붕붕,빙봉,붕붕:false", "프리코스:true", "myFancyCar:false", ",super:false"}, delimiter = ':')
     @DisplayName("입력한 이름이 올바른가")
     void emptyName(String userInput, boolean expected) {
         List<String> participants = Arrays.asList(userInput.split(","));
         
         assertThat(ValidationUtil.validName(participants)).isEqualTo(expected);
+    }
+    
+    @ParameterizedTest
+    @CsvSource(value = {"'':false", "문자:false", "alpha:false", "car1:false", "-1:false", "0:false", "1:true", "17:true"}, delimiter = ':')
+    @DisplayName("이동 횟수 값으로 올바른 값을 입력 했는가")
+    void validMoveCountInput(String userInput, boolean expected) {
+        assertThat(ValidationUtil.validMoveCount(userInput)).isEqualTo(expected);
     }
 }
